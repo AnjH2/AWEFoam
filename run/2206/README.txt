193ba4ed9ee276fea7553a7d4fb2afc4e310fab4
ANJacobsen <anj@mpe.au.dk>
2026-08-28 14:14:52 +0200

    removed unnecessary folders

--- run/2206/README.txt
+++
@@ -1,3 +0,0 @@
-Share cases for debugging purposes.
-Cases may not have any unnecessary data, and should have an “allRun” and “allClean” file.
-
--- scripts/paper3/openfoam/snappyHexMesh/jobChain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-jid1=$(sbatch --parsable step1.sh)
-jid2=$(sbatch --parsable --dependency=afterok:$jid1 step3.sh)
-jid3=$(sbatch --parsable --dependency=afterok:$jid2 reRun.sh)
-jid4=$(sbatch --parsable --dependency=afterok:$jid3 reRun.sh)
-echo "Submitted: $jid1 -> $jid2 -> $jid3 -> $jid4"
